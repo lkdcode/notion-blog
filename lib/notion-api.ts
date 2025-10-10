639899cf79a2c21b7c5ddc88dcdcfec6999d1bf2
lkdcode <dlrlejr1@naver.com>
2025-10-10 23:42:57 +0900

    Fix(:api) notion api 버전 변경

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,5 +1,6 @@
 import { NotionAPI } from 'notion-client'
 
 export const notion = new NotionAPI({
-  apiBaseUrl: process.env.NOTION_API_BASE_URL
+  // apiBaseUrl: process.env.NOTION_API_BASE_URL
+  apiBaseUrl: 'https://regal-candytuft-da4.notion.site/api/v3'
 })
